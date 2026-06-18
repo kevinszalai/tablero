7ecc5fc357591a092ad55f89ac0366c52fc68ad0
kevinszalai <kevinszalai@hotmail.com>
2026-06-17 23:00:15 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Estrategia: red primero, con copia en caché como respaldo.
    Así siempre te llevás la última versión del repo, pero si no hay
    internet la app abre igual con lo último que vio. */
-const CACHE = 'tablero-ks-v17';
+const CACHE = 'tablero-ks-v18';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 
 self.addEventListener('install', e => {
